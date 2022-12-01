fbeadb58348570e7f478b680e552f7d45f5a6deb
Pierre Sicallac <pierre.sicallac@hotmail.fr>
2022-12-01 07:17:16 +0100

    Starting 2022 year with love

---
+++ 2022/python/01/challenge_one.py
@@ -0,0 +1,16 @@
+with open('data.txt', 'r') as file:
+    elf= []
+    elf.append(0)
+    elf_num: int = 0
+    elf_cal = 0
+    for line in file:
+        if line == "\n":
+            elf[elf_num] = elf_cal
+            elf_cal = 0
+            elf.append(0)
+            elf_num += 1
+        else:
+            elf_cal += int(line)
+    
+    print(f"The higest cal carying by one Elf is {max(elf)}")
+        
