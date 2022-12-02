9bc8f24dd2c8186092811a45f61a92f430959d91
Pierre Sicallac <pierre.sicallac@hotmail.fr>
2022-12-02 15:01:00 +0100

    Add files via upload

---
+++ 2015/04/challenge_one.py
@@ -0,0 +1,21 @@
+from hashlib import md5
+import encodings
+
+SECRET_KEY = "bgvyzdsv" #Puzzle input
+
+def main():
+    increment = 0
+    while True:
+        
+        KEY = f"{SECRET_KEY}{str(increment)}"
+        KEY = bytes(KEY, encoding="utf8")
+        
+        hash_result = md5(KEY).hexdigest()
+        if hash_result[:5] == "00000":
+            print(increment, KEY, hash_result)
+            break
+        else:
+            increment += 1
+
+if __name__ == "__main__":
+    main()
